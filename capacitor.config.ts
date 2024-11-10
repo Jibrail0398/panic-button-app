fc0033115b4c3d9026e56759fff1eb9787fbd672
jibrail0398 <jibrailadji0398@gmail.com>
2024-11-10 22:06:47 +0700

    first commit

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'io.ionic.panic-button',
   appName: 'panic-button-app',
   webDir: 'www'
 };
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import { Geolocation } from '@capacitor/geolocation';
+
+
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
+
 export class HomePage {
-
+  
   constructor() {}
+  
+  latitude:number = 0;
+  longitude:number = 0;
 
+  getMyLocation = async () =>{
+    let coordinates = await Geolocation.getCurrentPosition();
+    this.latitude = coordinates.coords.latitude
+    this.longitude = coordinates.coords.longitude
+    console.log('Current position:', coordinates);
+  }
+  
+  
 }
