3a2e4b24cf2d2a3f87bd671c53d6aeb709e20bd1
jibrail0398 <jibrailadji0398@gmail.com>
2024-11-13 10:37:30 +0700

    Melakukan build android

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.panic-button',
+  appId: 'io.panicButton.starter',
   appName: 'panic-button-app',
   webDir: 'www'
 };
