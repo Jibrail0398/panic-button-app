b1730c8b6a3c750f5b0c70f82260aeb6aae01820
jibrail0398 <jibrailadji0398@gmail.com>
2025-03-05 12:03:05 +0700

    selesai halaman panic button

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,8 @@ module.exports = {
     extend: {
       colors:{
         'reddamkar':"#FE0000",
+        'button-red-100':'#FF4141',
+        'button-red-200':'#E40000',
         'input-text':'#F6F7F7'
       }
     },
