e881ac9e9f1b9ae62421ee06a02dd05734bcd9be
jibrail0398 <jibrailadji0398@gmail.com>
2025-03-23 14:31:21 +0700

    add cordova plugin

--- src/app/service/camera/camera.service.ts
+++ src/app/service/camera/camera.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-
+import { MediaCapture, MediaFile, CaptureVideoOptions, CaptureError } from '@awesome-cordova-plugins/media-capture/ngx';
 
 
 @Injectable({
@@ -7,10 +7,27 @@ import { Injectable } from '@angular/core';
 })
 export class CameraService {
 
-  constructor() { }
+  constructor(
+    private mediaCapture: MediaCapture
+  ) { }
 
-  async recordVideo() {
-    
+  async recordVideo(){
+    try {
+      const options: CaptureVideoOptions = {
+        limit: 1,
+        duration: 60, 
+        quality: 1,
+      };
+
+      const videoData= await this.mediaCapture.captureVideo(options);
+      console.log(videoData)
+      // const videoPath = videoData.fullPath;
+      // console.log('Video path:', videoPath);
+      // return videoPath;
+    } catch (error) {
+      console.error('Gagal merekam video:', error);
+      // return null;
+    }
   }
 
 }
