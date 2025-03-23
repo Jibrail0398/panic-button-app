ef87b78590dbaa6c933986088d406f659c40aa8d
jibrail0398 <jibrailadji0398@gmail.com>
2025-03-23 13:46:14 +0700

    remove camera plugin

--- src/app/service/camera/camera.service.ts
+++ src/app/service/camera/camera.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
+
 
 
 @Injectable({
@@ -10,18 +10,7 @@ export class CameraService {
   constructor() { }
 
   async recordVideo() {
-    try {
-      const video = await Camera.getPhoto({
-        source: CameraSource.Camera,
-        resultType: CameraResultType.Uri,
-      });
-
-      const videoPath = video.path || video.webPath;
-      return videoPath;
-    } catch (error) {
-      console.error('Gagal merekam video:', error);
-      return null;
-    }
+    
   }
 
 }
