a26bf839e29a2315a6aa5be4012db8aa55861e8b
jibrail0398 <jibrailadji0398@gmail.com>
2025-03-23 14:43:36 +0700

    Revert "remove camera plugin"
    This reverts commit ef87b78590dbaa6c933986088d406f659c40aa8d.

--- src/app/service/camera/camera.service.ts
+++ src/app/service/camera/camera.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { MediaCapture, MediaFile, CaptureVideoOptions, CaptureError } from '@awesome-cordova-plugins/media-capture/ngx';
+
 
 
 @Injectable({
@@ -8,26 +8,11 @@ import { MediaCapture, MediaFile, CaptureVideoOptions, CaptureError } from '@awe
 export class CameraService {
 
   constructor(
-    private mediaCapture: MediaCapture
+    
   ) { }
 
-  async recordVideo(){
-    try {
-      const options: CaptureVideoOptions = {
-        limit: 1,
-        duration: 60, 
-        quality: 1,
-      };
-
-      const videoData= await this.mediaCapture.captureVideo(options);
-      console.log(videoData)
-      // const videoPath = videoData.fullPath;
-      // console.log('Video path:', videoPath);
-      // return videoPath;
-    } catch (error) {
-      console.error('Gagal merekam video:', error);
-      // return null;
-    }
+  async recordVideo() {
+    
   }
 
 }
